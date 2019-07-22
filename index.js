7db92f140bc6572b9c91cfb321feac0bbfc293cc
Juanas Smith <juanas@giviews.com>
2019-07-22 23:25:16 +0700

    penambahan api films

--- index.js
+++ index.js
@@ -10,19 +10,17 @@ document.addEventListener("DOMContentLoaded", function() {
 		.then(data => {
 			console.log('hey ini respon API nya', data)
 
-			data forEach(element => {
+			data.forEach(element => {
 				var div = document.createElement("div");
 
-				var att = document.createTextNode("class");
-				div.setAttributeNodes(att)
-				att.value = "notification"
-				div.appendChild(node);
+				const att = document.createAttribute("class");       // Create a "class" attribute
+		        att.value = "notification is-primary";
+		        div.setAttributeNode(att);
 
-				var element = document.getElementById("div");
-				element.appendChild(div);
-			})
-
-			
+				const text = document.createTextNode(element.title + '\n'+'\n' + element.description +'\n'+ element.producer);
+				// const text = document.createTextNode(element.description);
+		        div.appendChild(text);
+		        app.appendChild(div);
+			});
 		})
-
 });
